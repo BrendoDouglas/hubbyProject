449f63401a567a4eced09525be1c7118e7126c7a
BrendoDouglas <brendodouglasmo@hotmail.com>
2022-11-07 10:04:13 -0300

    criado layout pagina "ver mais"

--- pages/userProfile/index.js
+++ pages/userProfile/index.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, Image, Pressable } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import styles from "./styles";
+import user64 from "../../assets/user64.png";
+import pointer16 from "../../assets/pointer16.png";
+import x16 from "../../assets/x16.png";
+import star16 from "../../assets/star16.png";
+import view24 from "../../assets/view24.png";
+import addfile24 from "../../assets/addfile24.png";
+import badge24 from "../../assets/badge24.png";
+import bell24 from "../../assets/bell24.png";
+import preferences24 from "../../assets/preferences24.png";
+import lupa24 from "../../assets/lupa24.png";
+import group24 from "../../assets/group24.png";
+import info24 from "../../assets/info24.png";
+import question24 from "../../assets/question24.png";
+import settings24 from "../../assets/settings24.png";
 
-export default function App() {
+export default function UserProfile() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <LinearGradient
+      colors={['#014442', '#78A967']}
+      start={{x: 0, y: 0}}
+      end={{x:1, y:1}}
+      style={{flex:1, padding:'10%'}}>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      <Pressable>
+        <Image source={x16} style={{width:16, height:16}}/>
+      </Pressable>
+
+      <View style={styles.container}>
+        <Image source={user64} style={{width:64, height:64}}/>
+        <Text style={styles.titleWhite}>Aline de Campos</Text>
+        <View style={styles.vImageText}>
+          <Image source={pointer16} style={{width:16, height:16}}/>
+          <Text style={styles.textWhite}> Porto Alegre, RS</Text>
+        </View>
+        <View style={styles.vImageText}>
+          <Image source={star16} style={{width:16, height:16}}/>
+          <Text style={[styles.textWhite, {fontSize:18}]}> 5.0 </Text>
+        </View>
+      </View>
+
+      <View style={{flex:1}}>
+        <Pressable style={styles.Pressable}>
+          <Image source={view24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Ver Histórico de atividade</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={addfile24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Criar evento</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={badge24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Avaliar usuários recentes</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={bell24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Notificações</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={preferences24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Preferências</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={lupa24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Buscar sala</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={group24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Amigos</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={info24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Sobre</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={question24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Ajuda</Text>
+        </Pressable>
+        <Pressable style={styles.Pressable}>
+          <Image source={settings24} style={{width:24, height:24}}/>
+          <Text style={styles.textButton}>Configurações</Text>
+        </Pressable>
+      </View>
+
+    </LinearGradient>
+  );
+};
